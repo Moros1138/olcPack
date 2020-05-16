0428d584807daac01825196134d74268bdf60871
Moros1138 <moros1138@gmail.com>
2020-05-16 10:48:45 -0400

    add license comments

--- src/olcResourcePack.h
+++ src/olcResourcePack.h
@@ -1,3 +1,67 @@
+/*
+	olcResourcePack.h
+
+	+-------------------------------------------------------------+
+	|           OneLoneCoder Resource Pack v1.00                  |
+	|  "When it's not good enough, make it better!!!" - Moros1138 |
+	+-------------------------------------------------------------+
+
+	What is this?
+	~~~~~~~~~~~~~
+	olc::ResourcePack is a complete rip, with extended functionality of
+	the ResourcePack class provided by the olc::PixelGameEngine by javidx9.
+
+	License (OLC-3)
+	~~~~~~~~~~~~~~~
+
+	Copyright 2018 - 2020 OneLoneCoder.com
+
+	Redistribution and use in source and binary forms, with or without modification,
+	are permitted provided that the following conditions are met:
+
+	1. Redistributions or derivations of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions or derivative works in binary form must reproduce the above
+	copyright notice. This list of conditions and the following	disclaimer must be
+	reproduced in the documentation and/or other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of its contributors may
+	be used to endorse or promote products derived from this software without specific
+	prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS	"AS IS" AND ANY
+	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+	SHALL THE COPYRIGHT	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+	INCIDENTAL,	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+	BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+	SUCH DAMAGE.
+
+	Links
+	~~~~~
+	YouTube:    https://www.youtube.com/channel/UCcSyeMr3dvXo-R2XNVNRrKA
+	GitHub:		https://www.github.com/moros1138
+	Homepage:	https://www.moros1138.com
+	
+	Author
+	~~~~~~
+	Moros Smith aka Moros1138
+
+	OneLoneCoder Links
+	~~~~~~
+	YouTube:	https://www.youtube.com/javidx9
+				https://www.youtube.com/javidx9extra
+	GitHub:		https://www.github.com/onelonecoder
+
+	Contributors
+	~~~~~~
+	David Barr aka javidx9
+*/
+
 #include <string>
 #include <vector>
 #include <list>
@@ -73,8 +137,6 @@ namespace olc {
 		std::list<std::string> ListFiles();
 		olc::rcode RemoveFile(const std::string &sFilename);
 		olc::rcode RenameFile(const std::string &src, const std::string &dest);
-
-
 	};
 
 }
